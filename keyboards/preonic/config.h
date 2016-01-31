a2d791f72fcdbccca8a1cf4b674d5d9b010489df
Loïc Guitaut <flink@belfalas.eu>
2016-01-31 23:07:42 +0100

    Use only shift under the thumb
    Restore the default 200ms for keystrokes.

--- keyboards/preonic/config.h
+++ keyboards/preonic/config.h
@@ -53,7 +53,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* Set 0 if debouncing isn't needed */
 #define DEBOUNCING_DELAY 5
-#define TAPPING_TERM    175
+#define TAPPING_TERM    200
 
 /* Mechanical locking support. Use KC_LCAP, KC_LNUM or KC_LSCR instead in keymap */
 #define LOCKING_SUPPORT_ENABLE
