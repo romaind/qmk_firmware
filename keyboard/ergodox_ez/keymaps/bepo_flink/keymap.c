41a60d2b5142c42de012ec4193f655845dffd18f
Loïc Guitaut <flink@belfalas.eu>
2016-01-31 02:48:17 +0100

    Test with a shift under the thumb

--- keyboard/ergodox_ez/keymaps/bepo_flink/keymap.c
+++ keyboard/ergodox_ez/keymaps/bepo_flink/keymap.c
@@ -16,15 +16,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * |Esc/Sft |   A  |   S  |   D  |   F  |   G  |------|           |------|   C  |   T  |   S  |   R  |   N  | M/Shift|
  * |--------+------+------+------+------+------| Enter|           |Enter |------+------+------+------+------+--------|
- * | Ê/Shift|   À  |   Y  |   X  |   .  |   K  |      |           |      |   '  |   Q  |   G  |   H  |   F  | Ç/Shift|
+ * |   Ê    |   À  |   Y  |   X  |   .  |   K  |      |           |      |   '  |   Q  |   G  |   H  |   F  |   Ç    |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   | Ctrl |      |      |      | Alt  |                                       |   W  |   %  |      |      | Ctrl |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
- *                                        |      | LGui |       | LGui |  Calc  |
+ *                                        | LGui |      |       | Calc |  LGui  |
  *                                 ,------|------|------|       |------+--------+------.
  *                                 |      |      |      |       |      |        |      |
- *                                 | Space| Ctrl |------|       |------|  Ctrl  | AltGr|
+ *                                 | Space| Ctrl |------|       |------| LShift | AltGr|
  *                                 |      |      |TMP L1|       |TMP L1|        |      |
  *                                 `--------------------'       `----------------------'
  */
@@ -35,20 +35,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_GRV,         KC_1,         KC_2,   KC_3,   KC_4,   KC_5,   KC_DELT,
         KC_TAB,         KC_Q,         KC_W,   KC_E,   KC_R,   KC_T,   KC_BSPC,
         SFT_T(KC_ESC),  KC_A,         KC_S,   KC_D,   KC_F,   KC_G,
-        SFT_T(FR_LESS), KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_ENT,
+        FR_LESS,        KC_Z,         KC_X,   KC_C,   KC_V,   KC_B,   KC_ENT,
         KC_LCTL,        KC_NO,        KC_NO,  KC_NO,  KC_LALT,
-                                                    KC_NO,    KC_LGUI,
+                                                    KC_LGUI,  KC_NO,
                                                               KC_NO,
                                           KC_SPC, KC_LCTL, MO(SPFN),
         // right hand
              KC_DELT,     KC_6,   KC_7,   KC_8,   KC_9,   KC_0,             KC_MINS,
              KC_BSPC,     KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,             KC_LBRC,
                           KC_H,   KC_J,   KC_K,   KC_L,   KC_SCLN,          SFT_T(KC_QUOT),
-             KC_ENT,      KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,          SFT_T(FR_ASTR),
+             KC_ENT,      KC_N,   KC_M,   KC_COMM,KC_DOT, KC_SLSH,          FR_ASTR,
                                   FR_DLR, FR_EQL, KC_NO,  KC_NO,            KC_RCTL,
-             KC_LGUI,     KC_CALC,
+             KC_CALC,     KC_LGUI,
              KC_NO,
-             MO(SPFN), KC_RCTL,KC_RALT
+             MO(SPFN), KC_LSFT,KC_RALT
     ),
 /* Keymap 1: Symbol Layer
  *
@@ -61,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|      |           | VolDn|------+------+------+------+------+--------|
  * |        |      |      |      |      |      |      |           |      |      |      |      |      |      |        |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   |      |      |      |      |      |                                       |      |      |      |      |      |
+ *   |RESET |      |      |      |      |                                       |      |      |      |      | DEBUG|
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
  *                                        |      |      |       |      |      |
@@ -78,7 +78,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_TRNS,KC_F6  ,KC_F7,  KC_F8  ,KC_F9  ,KC_F10 ,KC_TRNS,
        KC_TRNS,KC_F11 ,KC_F12, KC_TRNS,KC_TRNS,KC_TRNS,
        KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
-       KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
+       RESET,  KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,
                                        KC_TRNS,KC_TRNS,
                                                KC_TRNS,
                                KC_TRNS,KC_TRNS,KC_TRNS,
@@ -87,7 +87,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
        KC_VOLU, KC_TRNS, KC_HOME,KC_UP,   KC_END,  KC_PGUP, KC_TRNS,
                 KC_TRNS, KC_LEFT,KC_DOWN, KC_RGHT, KC_PGDN, KC_TRNS,
        KC_VOLD, KC_TRNS, KC_TRNS,KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
-                         KC_TRNS,KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
+                         KC_TRNS,KC_TRNS, KC_TRNS, KC_TRNS, DEBUG,
        KC_TRNS, KC_TRNS,
        KC_TRNS,
        KC_TRNS, KC_TRNS, KC_TRNS
